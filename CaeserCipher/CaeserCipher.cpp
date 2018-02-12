fa22917e6cf2db24005651a74eeabe4076144eaf
tom cuthbertson <mrtomcuthbertson@gmail.com>
2018-02-12 21:19:31 +0000

    added decoer and menu

--- CaeserCipher/CaeserCipher.cpp
+++ CaeserCipher/CaeserCipher.cpp
@@ -7,18 +7,13 @@
 
 using namespace std;
 
-int main()
-{
-	char letter[26]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-	char letterNew[26];
-	std::string word;
-	std::string wordNew;
-	int key;
 
-	std::cout << "please insert cipher key:" <<std::endl;
-	std::cin >> key;
-	std::cout << "please insert word to cipher:" << std::endl;
-	std::cin >> word;
+std::string cipher(int key, std::string word)
+{
+	char letter[26]{ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
+	char letterNew[26];
+	std::string wordNew;
+
 
 	for (int i = 0; i < 26; i++)
 	{
@@ -38,9 +33,56 @@ int main()
 		}
 	}
 
-	
-	cout << word;
+	return word;
 
+}
+
+int main()
+{
+	std::string word;
+	std::string wordNew;
+	int key;
+	int menu=-1;
+
+	while (menu != 0)
+	{
+		std::cout << "for caeser cipher coder, input 1" << std::endl;
+		std::cout << "for caeser cipher decoder, input 2" << std::endl;
+		std::cout << "to quit, input 0" << std::endl;
+		std::cin >> menu;
+
+		if (menu == 1)													//cipher coder
+		{
+			std::cout << "please insert cipher key:" << std::endl;
+			std::cin >> key;
+			std::cout << "please insert word to cipher:" << std::endl;
+			std::cin >> word;
+			wordNew = cipher(key, word);
+
+			cout << "the coded word is:   " << wordNew << "\n" << endl;
+
+		}
+		else if (menu == 2)												//cipher decoder
+		{
+			std::cout << "please insert cipher key:" << std::endl;
+			std::cin >> key;
+			std::cout << "please insert word to decipher:" << std::endl;
+			std::cin >> word;
+			wordNew = cipher((-key), word);
+
+			cout << "the decoded word is:   "<< wordNew << "\n" << endl;
+		}
+		else if (menu == 0)												//exit
+		{
+			break;
+		}
+		else															//invalid input
+		{
+			std::cout << "invalid number enetered\n" << std::endl;
+		}
+
+	}
     return 0;
 }
+
 
